025cf0d6cf93917c093e2bb82937415d4ac830c5
Jiihyun <cwlgusc@gmail.com>
2025-05-05 17:20:06 +0900

    feat: make program flow and implement main method

--- src/main/kotlin/oncall/controller/ProgramController.kt
+++ src/main/kotlin/oncall/controller/ProgramController.kt
@@ -3,15 +3,22 @@ package oncall.controller
 import oncall.domain.DayOfWeek
 import oncall.domain.Month
 import oncall.domain.Schedule
+import oncall.domain.ScheduleMaker
 import oncall.domain.WorkerName
 import oncall.util.InputHandler
 import oncall.view.InputView
+import oncall.view.OutputView
 
 class ProgramController {
 
     fun run() {
         val monthAndStartDay = InputHandler.retryOnInvalidInput { readMonthAndStartDay() }
         val schedules = InputHandler.retryOnInvalidInput { readSchedules() }
+
+        val scheduleMaker = ScheduleMaker(schedules.first, schedules.second)
+        val schedule = scheduleMaker.makeSchedule(monthAndStartDay.first)
+
+        OutputView.showSchedule(monthAndStartDay.first.value, schedule)
     }
 
     private fun readMonthAndStartDay(): Pair<Month, DayOfWeek> {
